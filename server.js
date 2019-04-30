c18738ce9da3b22583d8bbecd10618621053a0f0
Strontiumsun <izzyhall25@gmail.com>
2019-04-30 14:30:49 -0500

    created new branch

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+var db = require("./models");
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,7 +17,25 @@ if (process.env.NODE_ENV === "production") {
 //app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wanderlust");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wanderlust", { useNewUrlParser: true });
+
+// db.Test.create({
+//     displayName: "Sam",
+//     img: "http://d2a5vhda9v4n4x.cloudfront.net/wp-content/uploads/2018/04/american-green-tree-frog.jpg",
+//     program: "Awesome Trip",
+//     country: "Italy",
+//     countryCode: "ITA",
+//     tripOrg: "University of Cool",
+//     review: "This was an awesome trip! I'm so glad I went....!",
+//     dateStart: "01/12/2016",
+//     dateEnd: "05/02/2016"
+// }).then(function (dbTest) {
+//     console.log(dbTest)
+// }).catch(function (err) {
+//     console.log(err.message);
+// });
+
+
 
 // Start the API server
 app.listen(PORT, function () {
