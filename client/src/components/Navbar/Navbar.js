551883b0454f5bd9c79d2eb977343805260b2f3f
Jaime Zepeda <nnk168@gmail.com>
2019-05-01 14:15:35 -0500

    added login form

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,22 +1,14 @@
 import React from "react";
 
 function NavTabs(props) {
+
+
     return (
-        // <nav>
-        //     <div className="nav-wrapper">
-        //         {/* <a href="#" className="brand-logo">Logo</a> */}
-        //         <ul id="nav-mobile" className="right hide-on-med-and-down">
-        //             <li><a href="sass.html">Sass</a></li>
-        //             <li><a href="badges.html">Components</a></li>
-        //             <li><a href="collapsible.html">JavaScript</a></li>
-        //         </ul>
-        //     </div>
-        // </nav>
 
-        <nav>
-            <div className="nav-wrapper responsive-text">
-
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <nav className="red">
+            <div className="nav-wrapper">
+                <a href="/" class="brand-logo">Wanderlust</a>
+                <ul className="right hide-on-med-and-down">
                     <li className="nav-item">
                         <a href="/review" onClick={() => props.handlePageChange("Review")}
                             className={props.currentPage === "Review" ? "nav-link active" : "nav-link"} >
@@ -39,6 +31,7 @@ function NavTabs(props) {
                 </ul>
             </div>
         </nav>
+
     );
 }
 
--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -4,9 +4,55 @@ class LoginForm extends Component {
 
     render() {
         return (
-            <div>
-                <h2>This is the login form</h2>
-            </div>
+            <main>
+                <center>
+                    {/* <img class="responsive-img" style="width: 250px;" src="https://i.imgur.com/ax0NCsK.gif" /> */}
+                    <div class="section"></div>
+
+                    <h5>Please, login into your account</h5>
+                    <div class="section"></div>
+
+                    <div class="container">
+                        <div class="z-depth-1 grey lighten-4 row" >
+
+                            <form class="col s12" method="post">
+                                <div class='row'>
+                                    <div class='col s12'>
+                                    </div>
+                                </div>
+
+                                <div class='row'>
+                                    <div class='input-field col s12'>
+                                        <input class='validate' type='email' name='email' id='email' />
+                                        <label for='email'>Enter your email</label>
+                                    </div>
+                                </div>
+
+                                <div class='row'>
+                                    <div class='input-field col s12'>
+                                        <input class='validate' type='password' name='password' id='password' />
+                                        <label for='password'>Enter your password</label>
+                                    </div>
+                                    <label >
+                                        <a class='red-text' href='#!'><b>Forgot Password?</b></a>
+                                    </label>
+                                </div>
+
+                                <br />
+                                <center>
+                                    <div class='row'>
+                                        <button type='submit' name='btn_login' class='col s12 btn btn-large waves-effect red'>Login</button>
+                                    </div>
+                                </center>
+                            </form>
+                        </div>
+                    </div>
+                    <a href="/signup" class="red-text">Create account</a>
+                </center>
+
+                <div class="section"></div>
+                <div class="section"></div>
+            </main>
         )
 
     }
