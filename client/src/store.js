33246cd575dccbf1052eb4e37aef60f870b0a0d1
Liz Lemon <nikorasu.dx@gmail.com>
2019-05-02 10:01:46 -0500

    downgraded react-redux version to 5.1.1 from 7...

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,34 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
-const initialState = {};
-const middleware = [thunk];
-const store = createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
-export default store;
+const logger = createLogger({
+    /* https://github.com/evgenyrodionov/redux-logger */
+    collapsed: true,
+    diff: true
+});
+
+const configureStore = function (initialState) {
+    return createStore(
+        rootReducer,
+        initialState,
+        composeWithDevTools(
+            /* logger must be the last middleware in chain to log actions */
+            applyMiddleware(thunk, logger)
+        )
+    );
+}
+
+// const initialState = {};
+// const middleware = [thunk];
+// const store = createStore(
+//     rootReducer,
+//     initialState,
+//     compose(
+//         applyMiddleware(...middleware),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+// );
+export default configureStore;
