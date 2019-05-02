462d80c5666e17cccc16518f30efb40d3c1584fd
Jaime Zepeda <nnk168@gmail.com>
2019-05-02 10:16:47 -0500

    changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3001;
 
 
 // Define middleware here
