b8fe98f46c6279d7c5663a70441d87c8ba90dee1
BlackMG27 <oike279@gmail.com>
2019-05-04 13:51:48 -0500

    added the barebones for the ReviewList and the ReviewListItem

--- client/src/components/ReviewList/ReviewListItem.js
+++ client/src/components/ReviewList/ReviewListItem.js
@@ -1,10 +1,20 @@
 import React, {Fragment} from 'react';
 
-function ReviewListItem() {
+function ReviewListItem(props) {
     return (
         <Fragment>
             <div className="col l2"></div>
-            <div className="col l8 s12"></div>
+            <div className="col l8 s12">
+                <div className="col l4 s12">
+                    <img/>
+                </div>
+                <div className="col l8 s12">
+                    <h2>{props.displayName}</h2>
+                    <h3>Country: {props.Country}</h3>
+                    <h3>Rating: {props.rating}</h3>
+                    <button className="btn">View Review</button>
+                </div>
+            </div>
             <div className="col l2"></div>
         </Fragment>
     )
