882381542ccbf396ba539247754bea5ea199d9c9
Strontiumsun <izzyhall25@gmail.com>
2019-05-05 14:21:51 -0500

    commiting this branch to create a new branch

--- controllers/codeController.js
+++ controllers/codeController.js
@@ -12,6 +12,14 @@ module.exports = {
                 let cCode = result.data[0].alpha2Code;
                 res.json(cCode)
             })
+    },
+    getGenericAvatar: function (req, res) {
+        var UrlStart = "https://ui-avatars.com/api/";
+        var newName = req.body.displayName;
+        console.log(newName);
+        var newAvatar = `${UrlStart}?name=${newName}`
+        res.json(newAvatar)
     }
+    // we might not actually need this on the backend
 }
 
