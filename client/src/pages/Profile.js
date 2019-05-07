40d373eaaa3a40f5134aad9eed462bd053d0f104
Liz Lemon <nikorasu.dx@gmail.com>
2019-05-06 22:27:24 -0500

    profile page connected to pop query

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../actions/authActions";
 import API from "../utils/API";
+import { Rating } from 'semantic-ui-react';
+import Moment from 'react-moment';
 
 
 
@@ -15,6 +17,12 @@ class Profile extends Component {
 
     cardStyle = {
         padding: "2rem"
+    }
+
+    imgStyle = {
+        height: "200px",
+        width: "200px",
+        borderRadius: "50%"
     }
 
     componentDidMount() {
@@ -44,13 +52,54 @@ class Profile extends Component {
                     <div className="row">
                         User Name: {this.state.username}
                     </div>
-                    <div className="row">
-                        {/* Review: {this.state.email} */}
-                    </div>
                 </div>
+
+                {/* If there is no data to be shown in the results section, give a message. Otherwise show cards for each book.  */}
+                {!this.state.review.length ? (<div className="card cyan lighter-5" style={this.cardStyle}><h5 className="text-center">No Reviews to Display</h5></div>) : (
+                    this.state.review.map((currentReview, id) => {
+                        return (
+                            <div className="card cyan lighter-5" style={this.cardStyle}>
+                                <div className="row">
+                                    <div className="col s12 m4"></div>
+                                    <div className="col s12 m4 center-align">
+
+                                        <img className="center-align" src={currentReview.img ? currentReview.img : `https://ui-avatars.com/api/?name=${currentReview.displayName}`} style={this.imgStyle} alt="photo" />
+                                        <h5>{currentReview.displayName}</h5>
+                                    </div>
+                                    <div className="col s12 m4"></div>
+                                </div>
+                                <div className="row">
+                                    <div className="col s12 m4"></div>
+                                    <div className="col s12 m4 center-align">
+                                        <ul>
+                                            <li>Program Name: {currentReview.program} </li>
+                                            <li>Rating:
+                                    <Rating maxRating={5} disabled="true" rating={currentReview.rating} /></li>
+                                            <li>Dates Traveled:{" "}
+                                                <Moment parse="YYYY/MM/DD hh:mm" format="MM/DD/YY">{currentReview.dateStart}</Moment> - <Moment parse="YYYY/MM/DD hh:mm" format="MM/DD/YY">{currentReview.dateEnd}</Moment></li>
+                                            <li>Country: {currentReview.country}</li>
+                                        </ul >
+                                    </div>
+                                    <div className="col s12 m4"></div>
+                                </div>
+                                <div className="row">
+                                    {currentReview.review}
+                                </div>
+                                <div className="row">
+                                    <a style={{
+                                        borderRadius: "3px",
+                                        letterSpacing: "1.5px",
+                                        marginTop: "1rem"
+                                    }} className="btn btn-large waves-effect waves-light hoverable blue accent-3 center-block" href={"/reviewlist/" + currentReview.countryCode}>Delete</a>
+                                </div>
+                            </div>
+                        )
+                    }
+                    ))}
             </div>
         )
     }
+
 
 }
 
@@ -58,9 +107,11 @@ Profile.propTypes = {
     logoutUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired
 };
+
 const mapStateToProps = state => ({
     auth: state.auth
 });
+
 export default connect(
     mapStateToProps,
     { logoutUser }
