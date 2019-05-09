0ab7f099b0c7abd0d2dcfe1bcc9c088b45698477
BlackMG27 <oike279@gmail.com>
2019-05-09 12:01:25 -0500

    added text and styles to the Landing Page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,13 +1,21 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import Logo from "./../img/logolarge.png"
 
 function LandingPage() {
     return (
         <div className="body-splash">
-            <img src={Logo}/>
-            <hr/>
-            <p>Wow it's great! It's our app!</p>
-            <div>Made by Ogechi + Nick + Jaime + Izzy</div>
+            <div>
+                <img className="header__logo" src={Logo}/>
+            </div>
+
+            <hr className="header__rule"/>
+            <p className="header__copy">A study abroad semester can be an incredible
+                experience. We want to make finding that incredible experience easier.
+                Wanderlust lets you travel the globe with just the click of the mouse. Use our
+                service to find real student’s reviews of study abroad trips in the countries
+                that you want to visit. Take a trip you trust with Wanderlust.</p>
+            <p class="contributors">Made by Ogechi + Nick + Jaime + Izzy</p>
+            <p className="header__caution">**Best Viewed in Landscape**</p>
         </div>
 
     )
